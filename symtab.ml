911723059a85a37c810e178a0d5baeb4d5080d56
Alex Feinberg <alex@strlen.net>
2011-12-10 21:48:40 -0800

    Make symbol table case insensitive

--- symtab.ml
+++ symtab.ml
@@ -3,11 +3,11 @@ type t = (string, Sexp.t) Hashtbl.t
 let create n = Hashtbl.create n
 
 let add tab name sym =
-  Hashtbl.add tab name sym
+  Hashtbl.add tab (String.uppercase name) sym
     
 let lookup tab name =
   try
-    Hashtbl.find tab name
+    Hashtbl.find tab (String.uppercase name)
   with
       Not_found -> Sexp.Null
 
